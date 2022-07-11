979d4b6a72836f3338e37c7d45b39c54321972a1
Omar Qureshi <omar.qureshi@sega.co.uk>
2022-07-11 15:48:17 +0100

    validation fixes

--- examples/ssh/yum_updates.js
+++ examples/ssh/yum_updates.js
@@ -1,15 +1,17 @@
-/**
- * This Driver extracts available updates on RHEL based operating system and
- * displays the following in a table
- * - Package Name
- * - New version
- * - Repository
- * */
+/* This Driver extracts available updates on RHEL based operating system and
+  * displays the following in a table
+  * - Package Name
+  * - New version
+  * - Repository
+*/
 
 var options = {
   'command': 'yum list updates',
-  'timeout': 12000
+  'timeout': 12000,
+  'username': D.device.username(),
+  'password': D.device.password(),
 }
+
 
 function success(output) {
   var table = D.createTable(
@@ -21,31 +23,29 @@ function success(output) {
   );
   var lines = output.split("\n");
   updatesStart = false;
-  lines.forEach(line, idx) {
+  lines.forEach(function (line, idx) {
     if (line == "Updated Packages") {
       updatesStart = true;
+
     }
     if (updatesStart) {
+      var items = record.split(/(  )+/).filter(function(el) { return el != ' '  });
       table.insertRecord(
-        parseRecord(line);
+        items[0], [items[1], items[2]]
+
       )
     }
-  }
+  });
 }
 
 function commandExecutionCallback(output, error){
-    console.info("Execution: ", output);
-    if (error) {
-      console.error("Error: ", error);
-      D.failure(D.errorType.GENERIC_ERROR);
-    } else {
-      successCallback(output);
-    }
-}
-
-function parseRecord(record) {
-  var items = record.split(/( )+/).filter(function(el) { return el != ' ' });
-  return items[0], [items[1], items[2]];
+  console.info("Execution: ", output);
+  if (error) {
+    console.error("Error: ", error);
+    D.failure(D.errorType.GENERIC_ERROR);
+  } else {
+    successCallback(output);
+  }
 }
 
 function validate() {
